d0f22bf52df9bbef383dbfa821c90ba3479f1acc
Alexander Karuzoo <kalex.codr@gmail.com>
2025-04-20 14:39:17 -0300

    add images and fix buttons in product item component

--- src/app/(public)/components/product-item.tsx
+++ src/app/(public)/components/product-item.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import product from "@/public/joystick.png";
 import { Stars } from "./stars";
 import Link from "next/link";
+import eyeIcon from "@/public/eye-icon.svg";
+import cartIcon from "@/public/cart-button-icon.svg";
 
 interface ProductItemProps {
   id: string | number;
@@ -11,12 +13,12 @@ export function ProductItem({ id }: ProductItemProps) {
   return (
     <div className="group flex flex-col h-full py-4 items-center justify-between">
       <Image src={product.src} alt="" width={180} height={180} />
-      <div className="flex flex-col justify-between items-center mt-4 w-full px-2 transition-all duration-300 ease-in-out opacity-100 group-hover:opacity-0 group-hover:translate-y-[110px]">
+      <div className="flex flex-col justify-between  mt-4 w-full px-2 transition-all duration-300 ease-in-out opacity-100 group-hover:opacity-0 group-hover:translate-y-[110px]">
         <div className="p-2">
-          <p className="text-second text-sm font-normal">Play games</p>
+          <p className="text-primary text-lg font-medium">Play games</p>
         </div>
         <div className="p-2">
-          <p className="text-second text-sm font-normal">$ 11,70</p>
+          <p className="text-gray-1 text-lg font-semibold">$ 11,70</p>
         </div>
         <div className="p-2">
           <Stars />
@@ -24,12 +26,12 @@ export function ProductItem({ id }: ProductItemProps) {
       </div>
       <div className="flex w-full justify-between px-2 gap-3  transition-all duration-300 ease-in-out opacity-0 translate-y-[85px] group-hover:flex group-hover:opacity-100 group-hover:translate-y-[-25px]">
         <button className="bg-button-cart flex flex-1 justify-between items-center text-gray-800 rounded-[1.25rem] h-[60px] p-5">
-          <p className="font-semibold text-md"> Add to cart </p>
-          <span>icon</span>
+          <p className="font-semibold"> Add to cart </p>
+          <Image src={cartIcon.src} alt="" width={32} height={32} />
         </button>
         <Link href={`/product/${id}`}>
           <button className="bg-button-cart text-gray-800 rounded-r-default h-[60px] min-w-[60px] p-5">
-            <span>icon</span>
+            <Image src={eyeIcon.src} alt="" width={24} height={24} />
           </button>
         </Link>
       </div>
