311a11b389586ee0d715818dd68828673db78bb1
Alexander Karuzoo <kalex.codr@gmail.com>
2025-04-27 20:55:19 -0300

    login page

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col mx-auto w-[90%] min-h-40">
+      <div className="flex items-center justify-center h-full">
+        <p>LOGIN PAGE</p>
+      </div>
+    </div>
+  );
+}
