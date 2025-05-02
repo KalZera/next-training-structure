2fde781865e23c58aa66d7c3e6c5e6c2f2004979
Alexander Karuzoo <kalex.codr@gmail.com>
2025-05-02 16:21:18 -0300

    first structure

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,3 +1,8 @@
 export default function Account() {
-  return <div className="flex flex-col mx-auto w-[90%]"></div>;
+  return (
+    <div className="flex flex-col mx-auto w-[90%]">
+      {/* add avatar */}
+      {/* add box info account */}
+    </div>
+  );
 }
