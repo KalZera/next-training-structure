a48496ec2a87c3a197b278e4f81769b19afb7588
Alexander Karuzoo <kalex.codr@gmail.com>
2025-05-05 21:44:03 -0300

    account content

---
+++ src/app/(private)/account/components/layout/account-content.tsx
@@ -0,0 +1,21 @@
+export default function AccountContent() {
+  return (
+    <div className="flex flex-col mx-auto w-[90%] my-6">
+      <h1 className="text-2xl font-bold">Account</h1>
+      <p className="text-gray-600">
+        Manage your account settings and preferences.
+      </p>
+      <div className="mt-4">
+        <h2 className="text-xl font-semibold">Profile Information</h2>
+      </div>
+      <div className="mt-4">
+        <p>Name</p>
+        <p>username</p>
+        <p>password</p>
+        <p>orders</p>
+        <p>sended</p>
+        <p>cupons</p>
+      </div>
+    </div>
+  );
+}
