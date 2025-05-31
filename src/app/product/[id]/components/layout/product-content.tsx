2bedf29367ca6ca3eb0b4b0fc91f883778d64d94
Alexander Karuzoo <kalex.codr@gmail.com>
2025-05-30 22:02:51 -0300

    adding get stripe product

--- src/app/product/[id]/components/layout/product-content.tsx
+++ src/app/product/[id]/components/layout/product-content.tsx
@@ -1,28 +1,43 @@
 import Image from "next/image";
 import { redirect } from "next/navigation";
-import { ProductItem } from "@/app/components/ui/product-item";
+// import { Productproduct } from "@/app/components/ui/product-product";
 import { InfoProduct } from "../../components/info-product";
 import { ListProducts } from "../../components/list-products";
 import { RegistrationProductInfo } from "../../components/registration-product-info";
 import { BoxDescription } from "../../components/box-description";
-import { mock } from "@/app/mock";
 import { Breadcrumbs } from "@/app/components/layout/breadcrumbs";
+
+import { stripe } from "@/utils/stripe";
+import type { Stripe } from "stripe";
+
 export default async function ProductContent({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 }) {
-  const { id } = await params;
+  const { id } = params;
+  const product = await stripe.products.retrieve(id, {
+    expand: ["default_price"],
+  });
 
-  const product = mock.find((item) => String(item.id) === id);
   if (!product) {
     return redirect("/404");
   }
-  const mainCategory = product?.categories[0] || "";
-  const productRelated = mock.filter((item) =>
-    (item?.categories ?? []).includes(mainCategory)
-  );
-  const { name, price, reviews, quantity, colors, sizes } = product;
+  const productParsed = {
+    id: product.id,
+    name: product.name,
+    price: ((product.default_price as Stripe.Price)?.unit_amount ?? 0) / 100,
+    image: product.images[0],
+    description: product.description as string,
+    sku: product.unit_label ?? null,
+    categories: [],
+    availability: null,
+    reviews: null,
+    colors: [product.metadata.color],
+    sizes: [product.metadata.size],
+    quantity: 1,
+    stars: 5,
+  };
   return (
     <div className="flex flex-col w-[90%] mx-auto">
       <div className="p-3">
@@ -31,7 +46,7 @@ export default async function ProductContent({
       <div className="grid grid-cols-2 p-3 mb-2 gap-3">
         <div className="border border-card rounded-[1.25rem] flex justify-center">
           <Image
-            src={product?.image.src}
+            src={productParsed.image}
             width={650}
             height={560}
             className="object-cover"
@@ -39,25 +54,22 @@ export default async function ProductContent({
           />
         </div>
         <div className="flex flex-col gap-4 justify-around">
-          <InfoProduct
-            id={id}
-            {...{ name, price, reviews, quantity, colors, sizes }}
-          />
+          <InfoProduct {...productParsed} />
         </div>
       </div>
       <div className="grid grid-cols-2 p-3 mb-2 gap-3">
         <ListProducts />
-        <RegistrationProductInfo />
+        <RegistrationProductInfo sku={productParsed.sku} />
       </div>
-      <BoxDescription />
-      <div className="p-3">
+      <BoxDescription description={productParsed.description} />
+      {/* <div className="p-3">
         <p className="text-2xl text-primary font-semibold">Related Products</p>
         <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 xl:grid-cols-4 gap-4">
-          {productRelated.map((item) => (
-            <ProductItem key={item.id} {...item} />
+          {productRelated.map((product) => (
+            <Productproduct key={product.id} {...product} />
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
