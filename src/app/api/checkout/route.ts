3ae996096db62ba14622b3e0a35bb4071ac4b17c
Alexander Karuzoo <kalex.codr@gmail.com>
2025-06-13 23:50:02 -0300

    create route test

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,29 +1,27 @@
-// import { stripe } from "@/utils/stripe";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "@/utils/stripe";
+import {NextResponse} from 'next/server'
 
-export async function POST(request: NextApiRequest, response: NextApiResponse) {
+export async function POST(request: Request) {
+  console.log("Request body:", { body: request });
   // const priceId = request.body.priceId;
-  // const successUrl = `${process.env.NEXT_URL}/success`;
-  // const cancelUrl = `${process.env.NEXT_URL}/`;
+  const successUrl = `${process.env.NEXT_URL}/success`;
+  const cancelUrl = `${process.env.NEXT_URL}/`;
 
-  // const checkoutSession = await stripe.checkout.sessions.create({
-  //   mode: "payment",
-  //   line_items: [
-  //     {
-  //       price: priceId,
-  //       quantity: 1,
-  //     },
-  //   ],
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: "payment",
+    line_items: [
+      {
+        price: 'price_1RTvTlRlX9IWN9B1omyWeXRd',
+        quantity: 1,
+      },
+    ],
 
-  //   success_url:successUrl,
-  //   cancel_url: cancelUrl
-  // });
-
-
-  return response.status(200).send({
-    message: "Checkout session created successfully",
+    success_url:successUrl,
+    cancel_url: cancelUrl
   });
+  
 
+  return NextResponse.redirect(new URL(checkoutSession.url))
   // .status(201).json({
   //   checkoutSession: 'checkoutSession.url',
   // });
